81fa47eb9caa8017b9ea046ea5b68ae2c9ea3b3c
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-10 21:49:34 +0530

    Modified string repeat section in exercise 10.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Java Practice Exercise 1
+The following are the classes
--- src/main/java/com/stackroute/practiceexercise1/StringRepeater.java
+++ src/main/java/com/stackroute/practiceexercise1/StringRepeater.java
@@ -2,9 +2,38 @@ package com.stackroute.practiceexercise1;
 
 import java.util.Scanner;
 
+/**
+ * Practice Exercise Question - 10
+ * Given 2 inputs , where input1 is string and input 2 is integer value, the last n characters should
+ * repeat n number of times in the output String.
+ */
 public class StringRepeater {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        /*Take input from the user*/
+        System.out.print("Enter string: ");
+        String input = s.next();
+        /*Initialize a string builder which is used to change the string*/
+        StringBuilder output = new StringBuilder(input);
+        /*Take number from user*/
+        System.out.print("Enter number: ");
+        int number = s.nextInt();
+        while (number > input.length()) {
+            System.out.println("Please enter number less than " + input.length() + ":");
+            number = s.nextInt();
+        }
+        /*Take substring from the given input string by subtracting
+        the total length of the string with the given number
+        */
+        String subString = output.substring(input.length() - number);
+        /*Append the substring to the output for given "number" of times*/
+        /*Uncomment this line for java version > 11*/
+        /*for (int i = 0; i < number; i++) {
+            output.append(subString);
+        }*/
+        /*For java >= 11 use these lines*/
+        output.append(subString.repeat(number));
+        System.out.println(output);
         /*Close the scanner*/
         s.close();
     }
